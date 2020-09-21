0580ab9a8fcccd60a7df737427d94a788c1504dd
= <=>
2020-09-21 14:55:17 +0200

    Feat: add href to products

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const data = require('./data.json');
 const app = express()
 const port = 8080
 
-const detailUrl = process.env.DETAIL_URL || 'http://localhost:8080'
+const detailUrl = process.env.DETAIL_URL || 'http://localhost:3000'
 
 app.use(express.static('public'))
 
@@ -17,12 +17,15 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    let list = data
+    let list = data.map(item => {
+        item.href = detailUrl + '/' + item.id
+        return item
+    })
     if (req.query.search) {
         const searchTerm = req.query.search.toLowerCase()
         list = data.filter(item => item.name.toLowerCase().includes(searchTerm))
     }
-    res.render('home', { search: req.query.search, products: list, detailUrl });
+    res.render('home', { search: req.query.search, products: list });
 })
 
 app.listen(port, () => {
