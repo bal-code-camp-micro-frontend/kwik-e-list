4e72d0674b48bd9b53855dcec2eb911c66dad9d8
= <=>
2020-09-21 14:38:33 +0200

    add template

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
 const express = require('express')
 const exphbs = require('express-handlebars');
+const data = require('./data.json');
 const app = express()
 const port = 3000
+
+const detailUrl = process.env.DETAIL_URL || 'http://localhost:8080'
 
 app.use(express.static('public'))
 
@@ -14,7 +17,12 @@ app.engine('hbs', exphbs({
 app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
-    res.render('home');
+    let list = data
+    if (req.query.search) {
+        const searchTerm = req.query.search.toLowerCase()
+        list = data.filter(item => item.name.toLowerCase().includes(searchTerm))
+    }
+    res.render('home', { search: req.query.search, products: list, detailUrl });
 })
 
 app.listen(port, () => {
