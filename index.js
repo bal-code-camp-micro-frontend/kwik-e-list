eb57854b48e439642b30d92a5a7036d7b24d3ef6
= <=>
2020-09-21 16:13:38 +0200

    Feat: add recoomendations

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+"use strict";
 
 const express = require('express')
 const exphbs = require('express-handlebars');
@@ -19,14 +20,39 @@ app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     let list = data.map(item => {
-        item.href = detailUrl + '/' + item.id
+        item.href = detailUrl + '/product/' + item.id
         return item
     })
     if (req.query.search) {
         const searchTerm = req.query.search.toLowerCase()
         list = data.filter(item => item.name.toLowerCase().includes(searchTerm))
     }
-    res.render('home', { search: req.query.search, products: list });
+    res.render('home', {
+        search: req.query.search,
+        products: list
+    });
+})
+
+app.get('/recommendations/:id', (req, res) => {
+    const id = req.params.id
+    if (!id) {
+        res.status(400).send('ID is missing')
+    }
+
+    const product = data.filter(d => d.id == id)[0]
+    if (!product) {
+        res.status(404).send('Product not found')
+    }
+    let list = data
+        .map(item => {
+            item.href = detailUrl + '/product/' + item.id
+            return item
+        })
+        .filter(item => item.type === product.type)
+        .slice(0, 4)
+    res.render('recommendations', {
+        products: list
+    });
 })
 
 app.get('/healthz', (_, res) => {
@@ -34,5 +60,6 @@ app.get('/healthz', (_, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Homepage => http://localhost:${port}`)
+    console.log(`Recommendations => http://localhost:${port}/recommendations/1`)
 })
