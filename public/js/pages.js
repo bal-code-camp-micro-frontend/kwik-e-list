5e7694c474f7f589b41034ac476b9bd9fc0770ac
= <=>
2020-09-23 10:14:13 +0200

    add list element

--- public/js/pages.js
+++ public/js/pages.js
@@ -7,7 +7,25 @@ listPagesTemplate.innerHTML = `
 <link href="/l/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection" />
 
 <l-header></l-header>
-<h1>welcome with the header</h1>
+<h1>Our Products</h1>
+
+<div class="card-list">
+    <div class="row">
+        <div class="col s12 m6 l4 xl3">
+            <div class="card medium grey lighten-3"></div>
+        </div>
+        <div class="col s12 m6 l4 xl3">
+            <div class="card medium grey lighten-3"></div>
+        </div>
+        <div class="col s12 m6 l4 xl3">
+            <div class="card medium grey lighten-3"></div>
+        </div>
+        <div class="col s12 m6 l4 xl3">
+            <div class="card medium grey lighten-3"></div>
+        </div>
+    </div>
+</div>
+
 <a href="/product/1">Product 1</a>
 <l-footer></l-footer>
 `;
@@ -17,6 +35,18 @@ class ListPages extends HTMLElement {
     connectedCallback() {
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(listPagesTemplate.content.cloneNode(true));
+
+        fetch('/l/api/product')
+            .then(r => r.json())
+            .then(d => this.renderCardItems(d))
+            .catch(e => console.error(e))
+    }
+
+    renderCardItems(list) {
+        const listHtml = list.map(item => {
+            return `<span>${item.name}</span>`
+        })
+        this.shadowRoot.querySelector('div.card-list').innerHTML = listHtml
     }
 }
 
